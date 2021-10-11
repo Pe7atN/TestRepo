1f26b2e3d17fcb8c5306a77a4d9cf00ae7c5a659
Miro <miroslavmihaylov75432@gmail.com>
2021-10-11 18:53:50 +0300

    Fix landing page

--- LandingPageScript.js
+++ LandingPageScript.js
@@ -1,26 +1,41 @@
-function Blue(name)
+// let TimeOut;
+
+function Enter(name)
 {
+    let c=document.getElementById(name+'Container')
     let h=document.getElementById(name+'Period');
-    let c=document.getElementById(name+'Container');
-    c.style.zIndex="2";
-    h.style.color='#3B9AF3';
+    c.style.width='57%';
+    h.style.color='#318DE2';
 
     let cont;
     if(name=='First')cont=document.getElementById('SecondContainer');
     else cont=document.getElementById('FirstContainer');
-    cont.style.filter='blur(4px)';
+    cont.style.filter='blur(5px)';  
+    cont.style.width='43%';
 }
 
-function White(name)
+function Leave(name)
 {
+    let c=document.getElementById(name+'Container')
     let h=document.getElementById(name+'Period');
-    let c=document.getElementById(name+'Container');
     h.style.color='white';
-   setTimeout(()=>{c.style.zIndex="1";console.log(2)},300)
+    c.style.width='50%';
 
-   let cont;
-   if(name=='First')cont=document.getElementById('SecondContainer');
-   else cont=document.getElementById('FirstContainer');
-   cont.style.filter='blur(0px)'
+    if(name=='First')cont=document.getElementById('SecondContainer');
+    else cont=document.getElementById('FirstContainer');
+    cont.style.width='50%';
+    cont.style.filter='';  
 }
 
+
+// let h=document.getElementById(name+'Period');
+// let c=document.getElementById(name+'Container');
+// c.style.zIndex="2";
+// h.style.color='#3B9AF3';
+
+// // clearTimeout(TimeOut);
+// let cont;
+// if(name=='First')cont=document.getElementById('SecondContainer');
+// else cont=document.getElementById('FirstContainer');
+// cont.style.zIndex="1";
+// cont.style.filter='blur(4px)';
