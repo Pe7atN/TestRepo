00dc4db1402244e70457db84b4116d7267e3a897
Miro7543 <miroslavmihaylov75432@gmail.com>
2021-11-11 11:37:44 +0200

    Change Landing Page css

--- LandingPageScript.js
+++ LandingPageScript.js
@@ -1,32 +1,13 @@
-// let TimeOut;
-// window.onload=function()
-// {
-
-// }
-
-function Enter(name)
+function Enter()
 {
-    let c=document.getElementById(name+'Container')
-    let h=document.getElementById(name+'Period');
-    c.style.width='55%';
-    // h.style.color='#DB222A';
-
-    let cont;
-    if(name=='First')cont=document.getElementById('SecondContainer');
-    else cont=document.getElementById('FirstContainer');
+    let cont=document.getElementById('FirstContainer');
     cont.style.filter='blur(5px)';  
     cont.style.width='45%';
 }
 
-function Leave(name)
+function Leave()
 {
-    let c=document.getElementById(name+'Container')
-    let h=document.getElementById(name+'Period');
-    h.style.color='white';
-    c.style.width='50%';
-
-    if(name=='First')cont=document.getElementById('SecondContainer');
-    else cont=document.getElementById('FirstContainer');
+    let cont=document.getElementById('FirstContainer');
     cont.style.width='50%';
     cont.style.filter='';  
 }
